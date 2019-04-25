06a44781e006ebd816a555eddd4fadb786f218dd
Travys Keto <traketo@gmail.com>
2019-04-25 14:44:09 -0500

    added working client and team sections

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,41 @@
-// on scroll effects
-$(window).scroll(function(){
-	$(".navscroll").toggleClass('scrolled', $(this).scrollTop() > 100);
-});
+jQuery(document).ready(function() {
+	// on scroll effects
+	$(window).scroll(function(){
+		$(".navscroll").toggleClass('scrolled', $(this).scrollTop() > 100);
+	});
 
+	// standard parallax
+	$('.parallax-window').parallax({imageSrc: 'images/Parallax-Purple.jpg'
+    });
+
+
+	// carousel
+	$('#carousel-clients').on('slide.bs.carousel', function (e) {
+
+	    /*
+	        CC 2.0 License Iatek LLC 2018
+	        Attribution required
+	    */
+	    var $e = $(e.relatedTarget);
+	    var idx = $e.index();
+	    var itemsPerSlide = 5;
+	    var totalItems = $('.carousel-item').length;
+	    
+	    if (idx >= totalItems-(itemsPerSlide-1)) {
+	        var it = itemsPerSlide - (totalItems - idx);
+	        for (var i=0; i<it; i++) {
+	            // append slides to end
+	            if (e.direction=="left") {
+	                $('.carousel-item').eq(i).appendTo('.carousel-inner');
+	            }
+	            else {
+	                $('.carousel-item').eq(0).appendTo('.carousel-inner');
+	            }
+	        }
+	    }
+	});
+	
+});
 
 // background video player
 function deferVideo() {   
@@ -42,31 +75,3 @@ function WidthChange(mq) {
 		}); 
 	}
 }
-
-
-// standard parallax
-$('.parallax-window').parallax({imageSrc: 'images/Parallax-Purple.jpg'});
-
-
-
-// client carousel
-$('#clientCarousel').carousel({
-  interval: 5000
-})
-
-$('.carousel .carousel-item').each(function(){
-    var next = $(this).next();
-    if (!next.length) {
-    next = $(this).siblings(':first');
-    }
-    next.children(':first-child').clone().appendTo($(this));
-    
-    for (var i=0;i<2;i++) {
-        next=next.next();
-        if (!next.length) {
-			next = $(this).siblings(':first');
-		}
-        
-        next.children(':first-child').clone().appendTo($(this));
-      }
-});
